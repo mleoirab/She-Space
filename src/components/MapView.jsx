1d31ad2cf7a107053de5b7073551c51e7fb84409
Puzzlesla <fatimabala@icloud.com>
2026-01-10 15:30:55 -0500

    feat: add CategoryToggles, MapView, and POI data with color constants

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { MapContainer, TileLayer, Popup, CircleMarker, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "../leafletFix";
 import { CATEGORY_COLORS } from "./CategoryToggles";
 
 function FitBounds({ places }) {
